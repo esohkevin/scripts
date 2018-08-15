c1e63acae99532e763e99929746e6da67572097b
Esoh Kevin <esohkevin4@gmail.com>
2018-08-15 06:32:39 +0300

    created a bash file from the text file

---
+++ sshKeys.sh
@@ -0,0 +1,15 @@
+#!/bin/usr/sh
+##This is my first script##
+#Create ssh keys on linux
+
+ssh-keygen -t rsa
+
+## git clone <the github repository you want to clone to your local computer>
+
+#Hit enter to save in default
+
+#Accept request (yes)
+
+#Enter a passphrase (password)
+
+
