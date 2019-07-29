ae74c36f4f51a8e09f3101f1a5a9f8ddd28d3acd
esohkevin <esohkevin4@gmail.com>
2019-07-29 22:46:58 +0000

    add files

--- plink2rehh.sh
+++ plink2rehh.sh
@@ -53,31 +53,61 @@ if [[ $data == "sub" ]]; then
           sed '1d' chr${3}${4}.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > chr${3}${4}.map
           sed 's/0/2/g' chr${3}${4}.haps > chr${3}${4}.hap
           
-       
-       elif [[ "$param" == "3" && $# != 4 ]]; then
-          echo """
-               Usage: ./plink2rehh.sh sub 3 <pop-name> <input-VCF> (whole genome with more than one chromosomes)
+       elif [[ "$param" == "3" && $# != 6 ]]; then
+            echo """
+               Usage: ./plink2rehh.sh sub 3 <out-file-name> <#chr> <sample-file> <input-VCF> (whole genome with more than one chromosomes)
+                        
+                        out-file-name: The output file name prefix
+                                 #chr: The number of chromosomes in the input VCF file
+				 sample-file: List of subsamples to include
+                            input-VCF: The input phased VCF dataset (e.g. data.vcf or data.vcf.gz)
           """
-        elif [[ "$param" == "3" && $# == 4 ]]; then
-          # Entire dataset with more than one chromosomes
-          plink2 \
-            --export hapslegend \
-            --vcf $4 \
-            --out $3 \
-            --keep $3.txt \
-            --double-id
-         
-          sed '1d' ${4}.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > ${4}.map
-          sed 's/0/2/g' ${4}.haps > ${4}.hap
-       
-       fi
+       elif [[ "$param" == "3" && $# == 6 ]]; then
+
+            for chr in `(seq 1 $4)`; do
+           
+	      # Entire dataset with more than one chromosomes
+
+              plink2 \
+                --export hapslegend \
+                --vcf $6 \
+                --chr $chr \
+                --keep $5 \
+                --out $3${chr} \
+                --double-id
+
+              # Set awk variables
+
+              a='$1"\\t"'
+              b="\"$chr\""
+              c='"\\t"$2"\\t"$4"\\t"$3'
+
+	      echo "{print `awk -v vara="$a" -v varb="$b" -v varc="$c" 'BEGIN{print vara varb varc}'`}" > awkProgFile.txt
+
+              sed '1d' ${3}${chr}.legend | \
+                      awk -f awkProgFile.txt > ${3}${chr}.map
+              sed 's/0/2/g' ${3}${chr}.haps > ${3}${chr}.hap
+            done
+
+	       for chr in `(seq 1 $4)`; do
+
+	           if [[ -f "${3}${chr}.map" ]]; then
+	     
+		       awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5}' ${3}${chr}.map; 
+	   
+		   fi
     
-       for file in *.log *.sample; do
+	       done > snp.info
+	   
+       fi 
+
+       for file in *.log *.sample *.legend *.haps awkProgFile.txt; do
            if [[ -f ${file} ]]; then
              rm ${file}
            fi
        done
-    
+
+      
     fi
 
 elif [[ $data == "all" ]]; then
@@ -130,28 +160,55 @@ elif [[ $data == "all" ]]; then
           sed 's/0/2/g' chr${3}${4}.haps > chr${3}${4}.hap
  
  
-       elif [[ "$param" == "3" && $# != 4 ]]; then
+       elif [[ "$param" == "3" && $# != 5 ]]; then
           echo """
-               Usage: ./plink2rehh.sh all 3 <pop-name> <input-VCF> (whole genome with more than one chromosomes)
+               Usage: ./plink2rehh.sh all 3 <out-file-name> <#chr> <input-VCF> (whole genome with more than one chromosomes)
+	       		
+	       		out-file-name: The output file name prefix
+	       		         #chr: The number of chromosomes in the input VCF file
+			    input-VCF: The input phased VCF dataset (e.g. data.vcf or data.vcf.gz)
           """
-        elif [[ "$param" == "3" && $# == 4 ]]; then
-          # Entire dataset with more than one chromosomes
+       elif [[ "$param" == "3" && $# == 5 ]]; then
+
+       for chr in `(seq 1 $4)`; do
+	# Entire dataset with more than one chromosomes
           plink2 \
             --export hapslegend \
-            --vcf $4 \
-            --out $3 \
+            --vcf $5 \
+	    --chr $chr \
+            --out $3${chr} \
             --double-id
 
-          sed '1d' ${4}.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > ${4}.map
-          sed 's/0/2/g' ${4}.haps > ${4}.hap
+	  # Set awk variables
+	  a='$1"\\t"'
+	  b="\"$chr\""	  
+	  c='"\\t"$2"\\t"$4"\\t"$3'
+
+	  echo "{print `awk -v vara="$a" -v varb="$b" -v varc="$c" 'BEGIN{print vara varb varc}'`}" > awkProgFile.txt
+
+          sed '1d' ${3}${chr}.legend | \
+		  awk -f awkProgFile.txt > ${3}${chr}.map
+          sed 's/0/2/g' ${3}${chr}.haps > ${3}${chr}.hap
+
+       done
 
        fi
 
-       for file in *.log *.sample; do
+       for file in *.log *.sample *.legend *.haps awkProgFile.txt; do
            if [[ -f ${file} ]]; then
              rm ${file}
            fi
        done
+
+          for chr in `(seq 1 $4)`; do
+
+              if [[ -f "${3}${chr}.map" ]]; then
+
+               awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5}' ${3}${chr}.map
+
+	      fi
+	   
+          done > snp.info
 
     fi
 
--- runScan.sh
+++ runScan.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-if [[ $# != 3 ]]; then
-   echo """
-	Usage: ./runScan.sh <.hap+.map-root> <chr#> <pop-name>
-"""
-else
-   Rscript runScan.R $1 $2 $3
+param="$1"
 
-   echo """
-        Done Scanning Genome! Results saved in iHSResult.txt, frqResults.txt, and chr11Signals.txt
-        Please check the 'chr11Signals.txt' file for genomic positions of interest and run the 'runEHH.sh' script for EHH
-"""
+if [[ $param == [12] ]]; then
+
+   if [[ $param == "1" && $# != 4 ]]; then
+      echo """
+	   Usage: ./scanPart.R 1 <.hap+.map-root> <chr#> <output>
+	   
+	        .hap+.map-root: The hap and map file prefix (e.g. for chrA.hap and chrA.map, enter chrA)
+		          chr#: The chromoseme number (e.g. 11 if you are working with chromosome 11)
+                        output: The output file name
+      """
+
+   elif [[ $param == "1" && $# == 4 ]]; then
+
+        Rscript scanPart.R $2 $3 $4
+
+   elif [[ $param == "2" && $# != 3 ]]; then
+        echo """
+             Usage: ./scanFull.R 2 <.hap+.map-root> <output>
+
+	        .hap+.map-root: The hap and map file prefix (e.g. for chrA.hap and chrA.map, enter chrA)
+		        output: The output file name
+        """
+
+   elif [[ $param == "2" && $# == 3 ]]; then
+
+        Rscript scanFull.R $2 $3
+  
+   fi
+
+else
+	echo """
+	     Usage: ./runScan.sh [1|2] (Enter either 1 or 2)
+		
+	     	1: If you are running a region of a single chromosome or a whole single chromosome
+		2: If you are running a whole genome scan of more than one chromosomes
+        """
 
 fi
