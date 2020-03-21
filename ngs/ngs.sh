b79de13af0754dbded25ac27ab14d4757ce78d7f
esohkevin <esohkevin4@gmail.com>
2020-03-21 13:20:34 +0000

    update_ngs.sh

--- ngs/ngs.sh
+++ ngs/ngs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 function usage() {
-	printf "Usage: %s [ options ]\n" $(basename $0);
+	printf "Usage: %s [ fq|trim|map ] [ options ]\n" $(basename $0);
 	echo -e """
 		NGS Pipeline
+
+		Enter 'fq' OR 'trim' OR 'map' OR all to run either FastQC OR Trimmomatic OR alignment/mapping or all steps.
+		Enter any two to run only those two steps
 
 		General Options:
 		  -p,--fqpath    <str>    :Path to fastq files. 
@@ -93,13 +96,13 @@ while true; do
     esac
     continue
 done
-if [[ "$ref" == NULL ]]; then
-   echo "ERROR: -r,--ref not provided! Exiting..."; 1>&2;
-   exit 1
-fi
+#if [[ "$ref" == NULL ]]; then
+#   echo "ERROR: -r,--ref not provided! Exiting..."; 1>&2;
+#   exit 1
+#fi
 
-ls ${dname}*_1.fastq.gz > fwd.txt || ls ${dname}*_1.fq.gz > fwd.txt || ls ${dname}*_1.fastq > fwd.txt || ls ${dname}*_1.fq > fwd.txt
-ls ${dname}*_2.fastq.gz > rev.txt || ls ${dname}*_2.fq.gz > rev.txt || ls ${dname}*_2.fastq > rev.txt || ls ${dname}*_2.fq > rev.txt
+for i in $(ls ${dname}*_1.fastq.gz || ls ${dname}*_1.fq.gz || ls ${dname}*_1.fastq || ls ${dname}*_1.fq); do basename $i; done > fwd.txt
+for i in $(ls ${dname}*_2.fastq.gz || ls ${dname}*_2.fq.gz || ls ${dname}*_2.fastq || ls ${dname}*_2.fq); do basename $i; done > rev.txt
 if [[ $? != 0 ]]; then
     echo "ERROR: No fastq file in the specified location Terminating..."
     sleep 1;
@@ -118,51 +121,49 @@ threads:	$t
 outFile:	$out
 ========================================================"""
     mkdir -p fastq paired unpaired aligned
-    paste fwd.txt rev.txt | awk '{print $1,$2,$1}' | sed 's/_1.fastq.gz//2' > forward_reverse.txt
+    paste fwd.txt rev.txt | awk -v d="${dname}" '{print d$1,d$2,$1}' | sed 's/_1.fastq.gz//2' > forward_reverse.txt
     awk '{print $1,$2}' forward_reverse.txt > fastq.input.txt
     id=forward_reverse.txt
-    awk -v d="${dname}" '{print d$1,d$2,"paired/"$1,"unpaired/"$1,"paired/"$1,"unpaired/"$1}' $id | sed 's/1.fastq.gz/fp.fastq.gz/2' |  sed 's/1.fastq.gz/fu.fastq.gz/2' | sed 's/1.fastq.gz/rp.fastq.gz/2' |  sed 's/1.fastq.gz/ru.fastq.gz/2' > trim.input.txt
-    awk '{print $3,$5}' trim.input.txt | sed 's/_1.fastq.gz/.sam/1' > align.input.txt
-    if [[ "$1" == "P" ]]; then
-       which parallel
-       if [[ $? != 0 ]]; then
-          echo "You have chosen to run jobs in parallel. However, GNU parallel does not appear to have been installed!"
-          echo "Please install it and rerun your command"
-          1>&2;
-          exit 1;
-       else
-          echo "Your jobs will be run in parallel"
-             cat $id | parallel --col-sep ' ' echo PE -phred33 {} ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10 LEADING:$leadx TRAILING:$trailx SLIDINGWINDOW:4:15 MINLEN:36 -threads $t | xargs -P10 -n15 trimmomatic
-          rm $id
-       fi
-    else
-       id=fastq.input.txt; odr="fastq/"
-       while read -r line; do
-           echo "FastQC"
-           fastqc -t $t $line -o $odr
-       done < $id
-       
-       id=trim.input.txt
-       while read -r line; do
-           echo "Trommomatic"
-           trimmomatic PE -phred33 $line ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10 LEADING:$leadx TRAILING:$trailx SLIDINGWINDOW:4:15 MINLEN:36 -threads $t
-       done < $id
-       
-       bwa index $ref
-       id=align.input.txt
-       while read -r line; do
-           echo "BWA"
-           bwa mem -t $t $ref $line -o aligned/$(awk '{print $1}' trim.input.txt | sed 's/_1.fastq.gz/.sam/1')
-       done < $id
-       for sam in aligned/$(awk '{print $1}' trim.input.txt | sed 's/_1.fastq.gz/.sam/1'); do
-           samtools view -h ${sam} -O BAM -o ${sam/.sam/.bam}
-           samtools sort -O BAM --reference $ref -@ $t -o ${sam/.sam/.sorted.bam} ${sam/.sam/.bam}
-           echo ${sam/.sam/.sorted.bam}
-       done > bam.list
-       bcftools mpileup --min-MQ 2 --thread $t -f $ref -Oz -o out.vcf.gz -b bam.list
-       bcftools index -f -t out.vcf.gz
-       bcftools call -mv --threads $t -Oz -o ${out}.vcf.gz out.vcf.gz
-       bcftools index -f -t ${out}.vcf.gz
-       rm $id out.vcf.gz aligned/*.sam
-    fi
+#    awk '{print $1,$2,"paired/"$1,"unpaired/"$1,"paired/"$1,"unpaired/"$1}' $id | sed 's/1.fastq.gz/fp.fastq.gz/2' |  sed 's/1.fastq.gz/fu.fastq.gz/2' | sed 's/1.fastq.gz/rp.fastq.gz/2' |  sed 's/1.fastq.gz/ru.fastq.gz/2' > trim.input.txt
+    awk '{print $1,$2,"paired/"$3"_fp.fastq.gz","unpaired/"$3"_fu.fastq.gz","paired/"$3"_rp.fastq.gz","unpaired/"$3"_ru.fastq.gz"}' $id > trim.input.txt
+    awk '{print $3,$5,$3}' trim.input.txt | sed 's/_fp.fastq.gz/.sam/2' | sed 's/paired\///3' |  awk '{print $1,$2,"-o","aligned/"$3}' > align.input.txt
+    while true; do
+      case "$1" in
+         fq)
+           id=fastq.input.txt; odr="fastq/"
+           while read -r line; do
+               echo "FastQC"
+               fastqc -t $t $line -o $odr
+           done < $id
+           ;;
+         trim)
+           id=trim.input.txt
+           while read -r line; do
+               echo "Trommomatic"
+               trimmomatic PE -phred33 $line ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10 LEADING:$leadx TRAILING:$trailx SLIDINGWINDOW:4:15 MINLEN:36 -threads $t
+           done < $id
+           ;;
+         map)
+           if [[ "$ref" == NULL ]]; then
+              echo "ERROR: -r,--ref not provided! Exiting..."; 1>&2;
+              exit 1
+           fi
+           bwa index $ref
+           id=align.input.txt
+           while read -r line; do
+               echo "BWA"
+               bwa mem -t $t $ref $line
+           done < $id
+           for sam in $(awk '{print $4}' align.input.txt); do
+               samtools view -h ${sam} -O BAM -o ${sam/.sam/.bam}
+               samtools sort -O BAM --reference $ref -@ $t -o ${sam/.sam/.sorted.bam} ${sam/.sam/.bam}
+               echo ${sam/.sam/.sorted.bam}
+           done > bam.list
+           bcftools mpileup --min-MQ 2 --thread $t -f $ref -Oz -o out.vcf.gz -b bam.list
+           bcftools index -f -t out.vcf.gz
+           bcftools call -mv --threads $t -Oz -o ${out}.vcf.gz out.vcf.gz
+           bcftools index -f -t ${out}.vcf.gz
+           rm $id out.vcf.gz aligned/*.sam
+      esac
+    done
 fi
